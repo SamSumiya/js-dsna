382ff1c5d281aa6172d5f246ca6ead2de0471fd3
chenerychen <60025491+chenerychen@users.noreply.github.com>
2022-01-23 19:09:56 -0800

    added graph depth first traversal with recursion

--- JS Algorithms/Graph_0_Print.js
+++ JS Algorithms/Graph_0_Print.js
@@ -8,17 +8,34 @@ const graph = {
 };
 
 
-// DFT solution
-function depthFirstPrint(graph, el) {
-  let stack = [el];
-  let output = [];
+// DFT Iterative solution
+// function depthFirstPrint(graph, el) {
+//   let stack = [el];
+//   let output = [];
 
-  while (stack.length > 0) {
-    const current = stack.pop();
-    output.push(current);
-    stack = [...stack, ...graph[current]];
+//   while (stack.length > 0) {
+//     const current = stack.pop();
+//     output.push(current);
+//     stack = [...stack, ...graph[current]];
+//   }
+//   return output;
+// }
+
+// DFT Recursive solution
+function depthFirstPrint(graph, src) {
+
+  console.log(src)
+
+  for (let neighbor of graph[src]) {
+    depthFirstPrint(graph, neighbor)
   }
-  return output;
+
 }
+
+// DFT Recursive Solution
+
+
+
+
 
 depthFirstPrint(graph, 'a');
